586a1c566b5476ed6f30dc3c79ad8349057030fe
Kirk Rohani <kirk.rohani@gmail.com>
2023-03-25 14:11:06 -0500

    Added layout, views, SafeAreaView for ios added styles for text and containers

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Bard AI</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <SafeAreaView style={styles.mainContainer}>
+      <View style={styles.topContainer}>
+        <Text style={styles.headerTextStyle}>Bard AI</Text>
+      </View>
+      <View style={styles.questionContainer}>
+        <Text style={styles.normalTextStyle}>Enter prompt here: </Text>
+      </View>
+      <View style={styles.responseContainer}>
+        <Text style={styles.normalTextStyle}>AI response here: </Text>
+      </View>
+      <View style={styles.footerContainer} />
+    </SafeAreaView>
+    <StatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  headerTextStyle: {
+    color: 'black',
+    fontSize: 20,
+    textAlign: 'left',
+  },
+ normalTextStyle: {
+    color: 'black',
+    fontSize: 15,
+    textAlign: 'left',
+    padding: 10
+  },
+  mainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'maroon',
+  },
+  topContainer: {
+    backgroundColor: 'white',
+    width: '100%',
+    height: '5%',
     alignItems: 'center',
+    justifyContent: 'center'
+  },
+  questionContainer: {
+    backgroundColor: 'lightgreen',
+    width: '100%',
+    height: '10%',
     justifyContent: 'center',
+
+  },
+  responseContainer: {
+    backgroundColor: 'lightblue',
+    width: '100%',
+    height: '80%'
+  },
+  footerContainer: {
+    backgroundColor: 'darkgreen',
+    width: '100%',
+    height: '5%'
   },
 });
